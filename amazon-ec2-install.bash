b4ae28959604550ab39057f34eb6624a79ba806a
Konnor Rogers <konnor5456@gmail.com>
2018-08-17 18:59:15 -0400

    Update amazon-ec2-install.bash

--- amazon-ec2-install.bash
+++ amazon-ec2-install.bash
@@ -23,7 +23,7 @@ chsh -s /usr/bin/zsh $USER
 
 echo 'installing rvm'
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-\curl -sSL https://get.rvm.io | bash -s stable --ruby -y
+\curl -sSL https://get.rvm.io | bash
 
 
 # reload the the shell to use rvm
