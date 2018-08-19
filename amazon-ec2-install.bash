30b1c38fa6f91af93eb4a2a57f874e047fc0fede
Konnor Rogers <konnor5456@gmail.com>
2018-08-19 13:37:19 -0400

    updated to allow for mosh use

--- amazon-ec2-install.bash
+++ amazon-ec2-install.bash
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+aws configure --profile konnor
+
 USER="ec2-user"
 yum update -y
 
-PACKAGE_LIST="curl git vim docker zsh gcc"
+PACKAGE_LIST="curl git vim docker zsh gcc protobuf-devel boost-devel libutempter-devel ncurses-devel zlib-devel perl-CPAN cpp make automake gcc-c++ protoconf-devel openssl-devel"
 
 for item in $PACKAGE_LIST; do
 	echo "installing $item"
 	yum install -y $item
 done
 
-
 cd ~
-wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
-tar -xzvf chruby-0.3.9.tar.gz
-cd chruby-0.3.9/
-./scripts/setup.sh
+git clone https://github.com/keithw/mosh
+cd mosh
+./autogen.sh 
+./configure
+make
+make install
 
 # install oh-my-zsh
 echo 'installing ohmyzsh'
@@ -25,3 +28,8 @@ cd ~
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
+cd ~
+wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
+tar -xzvf chruby-0.3.9.tar.gz
+cd chruby-0.3.9/
+./scripts/setup.sh
