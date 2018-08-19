7ec93c8a70060aca9bb8f104f6ddefef889eb679
Konnor Rogers <konnor5456@gmail.com>
2018-08-19 10:01:09 -0400

    Update amazon-ec2-install.bash

--- amazon-ec2-install.bash
+++ amazon-ec2-install.bash
@@ -15,7 +15,7 @@ cd ~
 wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
 tar -xzvf chruby-0.3.9.tar.gz
 cd chruby-0.3.9/
-sudo ./scripts/setup.sh
+./scripts/setup.sh
 
 # install oh-my-zsh
 echo 'installing ohmyzsh'
