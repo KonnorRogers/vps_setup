cf2822ce9433ddb0174234aefcedee694a23d457
Konnor Rogers <konnor5456@gmail.com>
2018-08-19 23:55:57 -0400

    Update amazon-ec2-install.bash

--- amazon-ec2-install.bash
+++ amazon-ec2-install.bash
@@ -32,6 +32,7 @@ cd ~
 wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz |
 tar -xzvf chruby-0.3.9.tar.gz |
 cd chruby-0.3.9/ |
+make |
 make install |
 
 # install color ls
