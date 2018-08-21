0f1f216ff30b6363fe1f0a51f0827b1f2e29c828
Konnor Rogers <konnor5456@gmail.com>
2018-08-20 22:49:20 -0400

    Update linode.bash

--- linode.bash
+++ linode.bash
@@ -1,1 +1,62 @@
+username="paramagicdev"
+add user $username
+add user -aG $username
+
+sudo apt update
+sudo apt upgrade -y
+
+PACKAGE_LIST="curl tmux git vim docker zsh rvm gnupg2 sqlite3 postgresql less"
+
+for item in $PACKAGE_LIST; do
+  sudo apt install $item -y
+fi
+
+# install pip
+curl -O https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py --user
+
+# Configure docker
+groupadd docker
+usermod -aG docker $USER
+usermod -aG docker $username
+systemctl enable docker
+chkconfig docker on
+systemctl restart docker.service
+
+# install mosh
+git clone https://github.com/keithw/mosh
+cd mosh
+./autogen.sh
+./configure
+make
+make install
+
+# install rvm
+gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+cd /tmp
+curl -sSL https://get.rvm.io -o rvm.sh
+cat /tmp/rvm.sh | bash -s stable --rails
+source ~/.rvm/scripts/rvm
+
+# install ruby
+rvm install ruby-2.5.1
+rvm use ruby-2.5.1
+
+gem install rails -v 5.2.0
+rvm gemset create $username
+rvm 2.5.1@"$username" --create
+
+# install nodejs for rails pipeline
+cd /tmp
+\curl -sSL https://deb.nodesource.com/setup_10.x -o nodejs.sh
+less nodejs.sh
+cat /tmp/nodejs.sh | sudo -E bash -
+sudo apt install -y nodejs
+
+# install colorls
+gem install colorls
+
+# install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp ~/vps-setup/.zshrc ~/.zshrc
 
