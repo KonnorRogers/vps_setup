0363572649d8fef721c00d2e72ac8bfd5093742d
Konnor Rogers <konnor5456@gmail.com>
2018-08-20 22:57:15 -0400

    Update linode.bash

--- linode.bash
+++ linode.bash
@@ -60,3 +60,9 @@ gem install colorls
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp ~/vps-setup/.zshrc ~/.zshrc
 
+ZSH_PLUGINS="~/.oh-my-zsh/custom/plugins"
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_PLUGINS/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_PLUGINS/zsh-syntax-highlighting
+
+source ~/.zshrc
+
