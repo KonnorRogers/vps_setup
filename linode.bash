2fc654452a85814bba6fcacc793ed7457f542177
paramagicdev <konnor5456@gmail.com>
2018-08-24 23:50:02 +0000

    Added neovim support

--- linode.bash
+++ linode.bash
@@ -16,7 +16,7 @@ su - paramagician
 sudo apt update
 sudo apt upgrade -y
 
-PACKAGE_LIST="curl tmux git vim zsh gnupg2 sqlite3 postgresql less python3 python-pip ufw neovim pry"
+PACKAGE_LIST="curl tmux git vim zsh gnupg2 sqlite3 postgresql less python3 python3-pip ufw neovim pry ack-grep libfuse2 fuse"
 
 
 for item in $PACKAGE_LIST; do
