e75225118e31027fdddb9df593ac918932b2918b
paramagicdev <konnor5456@gmail.com>
2018-08-27 07:09:05 +0000

    need to relook at username setup

--- linode.bash
+++ linode.bash
@@ -2,13 +2,13 @@ username="paramagician"
 email="konnor5456@gmail.com"
 
 
-if getent passwd $1 > /dev/null 2>&1; then
-    echo "$username is already being used!"
-else
-    echo "$username is not taken!"
-    adduser $username
-    adduser $username sudo
-fi
+# if getent passwd $1 > /dev/null 2>&1; then
+#    echo "$username is already being used!"
+# else
+#    echo "$username is not taken!"
+#    adduser $username
+#    adduser $username sudo
+# fi
 
 # change to user
 su - paramagician
