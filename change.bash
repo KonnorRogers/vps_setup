4e97996e22a068468c94609afb3ccc4a8df841a6
Konnor Rogers <konnor5456@gmail.com>
2018-08-30 00:48:34 -0400

    Create change.bash

---
+++ change.bash
@@ -0,0 +1,8 @@
+# tmux
+ln -s ~/vps-setup/tmux.conf ~/.tmux.conf
+# vim
+ln -s ~/vps-setup/vimrc ~/.vimrc
+# zsh
+ln -s ~/vps-setup/zshrc ~/.zshrc
+# neovim
+ln -s ~/vps-setup/nvim/init.vim ~/.config/nvim/init.vim
