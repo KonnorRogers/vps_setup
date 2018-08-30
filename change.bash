d365c854a13e116c1bb69959da36f056f4031844
paramagicdev <konnor5456@gmail.com>
2018-08-30 07:04:56 +0000

    added -f to symlinks

--- change.bash
+++ change.bash
@@ -1,10 +1,10 @@
 # install powerlevel9k
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+# git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 # tmux
-cp ~/vps-setup/tmux.conf ~/.tmux.conf
+ln -f -s ~/vps-setup/tmux.conf ~/.tmux.conf
 # vim
-cp ~/vps-setup/vimrc ~/.vimrc
+ln -f -s ~/vps-setup/vimrc ~/.vimrc
 # zsh
-cp ~/vps-setup/zshrc ~/.zshrc
+ln -f -s ~/vps-setup/zshrc ~/.zshrc
 # neovim
-cp ~/vps-setup/nvim/init.vim ~/.config/nvim/init.vim
+ln -f -s ~/vps-setup/nvim/init.vim ~/.config/nvim/init.vim
