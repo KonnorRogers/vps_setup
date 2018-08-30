8cbb36d4e92d3eab255a041fe89627b7b07fc505
paramagicdev <konnor5456@gmail.com>
2018-08-30 02:22:29 -0400

    Changed ln -s to cp

--- change.bash
+++ change.bash
@@ -1,10 +1,10 @@
 # install powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 # tmux
-ln -s ~/vps-setup/tmux.conf ~/.tmux.conf
+cp ~/vps-setup/tmux.conf ~/.tmux.conf
 # vim
-ln -s ~/vps-setup/vimrc ~/.vimrc
+cp ~/vps-setup/vimrc ~/.vimrc
 # zsh
-ln -s ~/vps-setup/zshrc ~/.zshrc
+cp ~/vps-setup/zshrc ~/.zshrc
 # neovim
-ln -s ~/vps-setup/nvim/init.vim ~/.config/nvim/init.vim
+cp ~/vps-setup/nvim/init.vim ~/.config/nvim/init.vim
