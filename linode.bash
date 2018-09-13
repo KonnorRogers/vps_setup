0bd5df9fab68c8eb3b0818bc59be71af973b478c
paramagicdev <konnor5456@gmail.com>
2018-09-13 01:32:03 +0000

    Changed dir to home prior to installing chruby

--- linode.bash
+++ linode.bash
@@ -81,6 +81,7 @@ systemctl enable docker
 systemctl restart docker.service
 
 # install chruby
+cd ~
 wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
 tar -xzvf chruby-0.3.9.tar.gz
 cd chruby-0.3.9/
