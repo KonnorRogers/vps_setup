db6eff82a84dd1d297e4600b06720456a54ceec0
paramagicdev <konnor5456@gmail.com>
2018-09-15 12:16:09 -0400

    changes to change.bash

--- change.bash
+++ change.bash
@@ -1,10 +1,8 @@
-# install powerlevel9k
-# git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-# tmux
-ln -f -s ~/vps-setup/tmux.conf ~/.tmux.conf
+#tmux
+cp ~/vps-setup/tmux.conf ~/.tmux.conf
 # vim
-ln -f -s ~/vps-setup/vimrc ~/.vimrc
+cp ~/vps-setup/vimrc ~/.vimrc
 # zsh
-ln -f -s ~/vps-setup/zshrc ~/.zshrc
+cp ~/vps-setup/zshrc ~/.zshrc
 # neovim
-ln -f -s ~/vps-setup/nvim/init.vim ~/.config/nvim/init.vim
+cp ~/vps-setup/nvim/init.vim ~/.config/nvim/init.vim
