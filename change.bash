9d1b5a4f5f34ce9d0f17cd9d89e5b203329ac74b
Konnor Rogers <konnor5456@gmail.com>
2018-09-19 20:45:49 -0400

    Update change.bash

--- change.bash
+++ change.bash
@@ -1,10 +1,14 @@
 #tmux
+cp ~/.tmux.conf ~/.tmux.conf.orig
 ln -f -s ~/vps-setup/tmux.conf ~/.tmux.conf
 # vim
+cp ~/.vimrc ~/.vimrc.orig
 ln -f -s ~/vps-setup/vimrc ~/.vimrc
 # zsh
+cp ~/.zshrc ~/.zshrc.orig
 ln -f -s ~/vps-setup/zshrc ~/.zshrc
 # neovim
 mkdir .config
 mkdir nvim
+cp ~/.config/nvim/init.vim ~/.config/nvim/init.vim.orig
 ln -f -s ~/vps-setup/nvim/init.vim ~/.config/nvim/init.vim
