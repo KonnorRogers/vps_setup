54e45c73ec4c68b94c2f3a86b2a033187038de0f
paramagicdev <konnor5456@gmail.com>
2018-09-21 14:32:28 -0400

    Changed change.bash to detect cygwin and transfer the appropriate zsh file.

--- change.bash
+++ change.bash
@@ -6,6 +6,7 @@ ln -f -s ~/vps-setup/tmux.conf ~/.tmux.conf
 # vim
 cp ~/.vimrc ~/.vimrc.orig
 ln -f -s ~/vps-setup/vimrc ~/.vimrc
+
 if [[ $OSTYPE == 'linux-gnu' ]]; then
     #zsh
     cp ~/.zshrc ~/.zshrc.orig
@@ -13,6 +14,11 @@ if [[ $OSTYPE == 'linux-gnu' ]]; then
     # neovim
     cp ~/.config/nvim/init.vim ~/.config/nvim/init.vim.orig
     ln -f -s ~/vps-setup/nvim/init.vim ~/.config/nvim/init.vim
+fi
+
+if [[ $OSTYPE == 'cygwin' ]]; then
+    cp ~/.zshrc ~/.zshrc.orig
+    ln -f -s ~/vps-setup/cygwin_zshrc ~/.zshrc
 fi
 
 echo 'dotfiles transferred successfully!'
