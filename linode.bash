b80861f11cddf8206034c6e9d8cf66fc7b6159d3
= <konnor5456@gmail.com>
2018-10-03 19:55:33 +0000

    adding eclim

--- linode.bash
+++ linode.bash
@@ -15,7 +15,7 @@ get_dependencies() {
     sudo apt upgrade -y 
     sudo apt autoremove -y 
     # Currently install python2/3, pip, tmux, vim, zsh, sqlite3, postgresql, golang, nodejs as well as other get_dependencies
-    PACKAGE_LIST="curl software-properties-common tmux git vim zsh gnupg2 sqlite3 postgresql less python3 python3-pip python-dev python3-dev python-pip ufw pry ack-grep libfuse2 fuse python3-neovim build-essential bison zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libffi-dev nodejs apt-transport-https ca-certificates golang oracle-java8-installer make gcc"
+    PACKAGE_LIST="curl software-properties-common tmux git vim zsh gnupg2 sqlite3 postgresql less python3 python3-pip python-dev python3-dev python-pip ufw pry ack-grep libfuse2 fuse python3-neovim build-essential bison zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libffi-dev nodejs apt-transport-https ca-certificates golang oracle-java8-installer make gcc xvfb"
 
     for item in $PACKAGE_LIST; do
       sudo apt -y install $item
