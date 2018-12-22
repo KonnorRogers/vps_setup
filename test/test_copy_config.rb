41723a99dcadad922150363e664eb0311a6240fa
= <konnor5456@gmail.com>
2018-12-22 19:12:24 +0000

    made backup and dest dirs constant, added a legit logger

--- test/test_copy_config.rb
+++ test/test_copy_config.rb
@@ -3,109 +3,117 @@
 require 'test_helper'
 require 'fileutils'
 require 'stringio'
+require 'logger'
 
 ROOT = File.expand_path(__dir__)
+BACKUP_DIR = File.join(ROOT, 'backup_dir')
+DEST_DIR = File.join(ROOT, 'dest_dir')
 
 LOG_PATH = File.join(ROOT, 'logs', "#{File.basename(__FILE__, '.rb')}.log")
-LOG_FILE = File.new(LOG_PATH, 'w+')
+LOG_FILE = Logger.new(LOG_PATH, '5')
+# LOG_FILE = File.new(LOG_PATH, 'w+')
 
 class TestCopyConfig < Minitest::Test
   def setup
     @console = capture_console
-    @backup_dir = File.join(ROOT, 'backup_dir')
-    @dest_dir = File.join(ROOT, 'dest_dir')
+    FileUtils.rm_rf(BACKUP_DIR)
+    FileUtils.rm_rf(DEST_DIR)
   end
 
   def teardown
-    File.open(LOG_FILE, File::APPEND) do
-      LOG_FILE.write("Beginning of test\n\n")
-      LOG_FILE.write(@console[:fake_err].string)
-      LOG_FILE.write(@console[:fake_out].string)
-      LOG_FILE.write("\nEND OF TEST\n\n")
-    end
+    # File.open(LOG_FILE, File::APPEND) do
+    #   LOG_FILE.write("Beginning of test\n\n")
+    #   LOG_FILE.write(@console[:fake_err].string)
+    #   LOG_FILE.write(@console[:fake_out].string)
+    #   LOG_FILE.write("\nEND OF TEST\n\n")
+    # end
+    error = Logger::ERROR
+    info = Logger::INFO
+    LOG_FILE.add(error, @console[:fake_err])
+    LOG_FILE.add(info, @console[:fake_out])
 
     restore_out_err(@console)
-    FileUtils.rm_rf(@backup_dir)
-    FileUtils.rm_rf(@dest_dir)
+    FileUtils.rm_rf(BACKUP_DIR)
+    FileUtils.rm_rf(DEST_DIR)
   end
 
   def test_creates_backup_dir_and_dest_dir
-    refute(Dir.exist?(@backup_dir))
-    refute(Dir.exist?(@dest_dir))
+    refute(Dir.exist?(BACKUP_DIR))
+    refute(Dir.exist?(DEST_DIR))
 
-    CopyConfig.copy(backup_dir: @backup_dir, dest_dir: @dest_dir)
+    CopyConfig.copy(backup_dir: BACKUP_DIR, dest_dir: DEST_DIR)
 
-    assert(Dir.exist?(@backup_dir))
-    assert(Dir.exist?(@dest_dir))
+    assert(Dir.exist?(BACKUP_DIR))
+    assert(Dir.exist?(DEST_DIR))
   end
 
   def test_will_not_error_if_backup_dir_and_dest_dir_exist
-    FileUtils.mkdir_p(@backup_dir)
-    FileUtils.mkdir_p(@dest_dir)
-    assert(Dir.exist?(@backup_dir))
-    assert(Dir.exist?(@dest_dir))
+    FileUtils.mkdir_p(BACKUP_DIR)
+    FileUtils.mkdir_p(DEST_DIR)
+    assert(Dir.exist?(BACKUP_DIR))
+    assert(Dir.exist?(DEST_DIR))
 
-    CopyConfig.copy(backup_dir: @backup_dir, dest_dir: @dest_dir)
-    assert(Dir.exist?(@backup_dir))
-    assert(Dir.exist?(@dest_dir))
+    CopyConfig.copy(backup_dir: BACKUP_DIR, dest_dir: DEST_DIR)
+    assert(Dir.exist?(BACKUP_DIR))
+    assert(Dir.exist?(DEST_DIR))
   end
 
   def test_backup_dir_empty_and_dest_dir_should_not_be_empty
-    CopyConfig.copy(backup_dir: @backup_dir, dest_dir: @dest_dir)
+    CopyConfig.copy(backup_dir: BACKUP_DIR, dest_dir: DEST_DIR)
     # Will not add files to the backup_dir if original dotfiles do not exist
-    assert_empty(Dir.children(@backup_dir))
+    assert_empty(Dir.children(BACKUP_DIR))
 
-    refute_empty(Dir.children(@dest_dir))
-    assert_includes(Dir.children(@dest_dir), '.vimrc')
+    refute_empty(Dir.children(DEST_DIR))
+    assert_includes(Dir.children(DEST_DIR), '.vimrc')
   end
 
   def test_backup_dir_not_empty_if_orig_found
-    FileUtils.mkdir_p(@dest_dir)
-    backup_file = File.join(@backup_dir, '.vimrc.orig')
-    dest_file = File.join(@dest_dir, '.vimrc')
+    FileUtils.mkdir_p(DEST_DIR)
+    backup_file = File.join(BACKUP_DIR, '.vimrc.orig')
+    dest_file = File.join(DEST_DIR, '.vimrc')
 
     File.open(dest_file, 'w+') { |file| file.puts 'test' }
     dest_file_before_copy = File.read(dest_file)
 
-    CopyConfig.copy(backup_dir: @backup_dir, dest_dir: @dest_dir)
+    CopyConfig.copy(backup_dir: BACKUP_DIR, dest_dir: DEST_DIR)
 
-    refute_empty(Dir.children(@backup_dir))
-    assert_includes(Dir.children(@backup_dir), '.vimrc.orig')
+    refute_empty(Dir.children(BACKUP_DIR))
+    assert_includes(Dir.children(BACKUP_DIR), '.vimrc.orig')
 
     assert dest_file_before_copy == File.read(backup_file)
     refute dest_file_before_copy == File.read(dest_file)
   end
 
   def test_backup_file_will_not_be_overwritten
-    FileUtils.mkdir_p(@dest_dir)
-    FileUtils.mkdir_p(@backup_dir)
-    f1 = File.join(@dest_dir, '.vimrc')
-    f2 = File.join(@backup_dir, '.vimrc.orig')
+    FileUtils.mkdir_p(DEST_DIR)
+    FileUtils.mkdir_p(BACKUP_DIR)
+    f1 = File.join(DEST_DIR, '.vimrc')
+    f2 = File.join(BACKUP_DIR, '.vimrc.orig')
     File.open(f1, 'w+') { |file| file.puts '1' }
     File.open(f2, 'w+') { |file| file.puts '2' }
-    CopyConfig.copy(backup_dir: @backup_dir, dest_dir: @dest_dir)
+    CopyConfig.copy(backup_dir: BACKUP_DIR, dest_dir: DEST_DIR)
     refute File.read(f1) == File.read(f2)
   end
 
   def test_config_file_will_be_copied_to_dest_dir
     config_file = File.join(CopyConfig::CONFIG_DIR, 'vimrc')
-    dest_file = File.join(@dest_dir, '.vimrc')
+    dest_file = File.join(DEST_DIR, '.vimrc')
 
     assert_raises(Errno::ENOENT) { File.read(config_file) == File.read(dest_file) }
 
-    FileUtils.mkdir_p(@dest_dir)
+    FileUtils.mkdir_p(DEST_DIR)
     File.open(dest_file, 'w+') { |file| file.puts 'test' }
     refute File.read(config_file) == File.read(dest_file)
 
-    CopyConfig.copy(backup_dir: @backup_dir, dest_dir: @dest_dir)
+    CopyConfig.copy(backup_dir: BACKUP_DIR, dest_dir: DEST_DIR)
 
     assert File.read(config_file) == File.read(dest_file)
   end
 
   def test_non_unix_files_not_copied
-    CopyConfig.copy(backup_dir: @backup_dir, dest_dir: @dest_dir)
+    CopyConfig.copy(backup_dir: BACKUP_DIR, dest_dir: DEST_DIR)
 
-    refute File.exist?(File.join(@dest_dir, '.minttyrc'))
-    refute File.exist?(File.join(@dest_dir, '.cygwin_zshrc'))
+    refute File.exist?(File.join(DEST_DIR, '.minttyrc'))
+    refute File.exist?(File.join(DEST_DIR, '.cygwin_zshrc'))
   end
 end
