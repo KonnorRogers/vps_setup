d38b37772019f4da6c2d900828d64a041d41f9f8
= <konnor5456@gmail.com>
2018-12-26 05:16:13 +0000

    all included into a module

--- lib/vps_setup/copy.rb
+++ lib/vps_setup/copy.rb
@@ -3,122 +3,124 @@
 require 'fileutils'
 require 'os' # returns users OS
 
-# Copies config from /vps-setup/config to your home dir
-class CopyConfig
+module VPS_Setup
+  # Copies config from /vps-setup/config to your home dir
+  class CopyConfig
 
-  def self.copy(backup_dir:, dest_dir:, attr: {})
-    attr[:posix] ||= OS.posix?
-    raise 'Please run from a posix platform' unless attr[:posix] == true
+    def self.copy(backup_dir:, dest_dir:, attr: {})
+      attr[:posix] ||= OS.posix?
+      raise 'Please run from a posix platform' unless attr[:posix] == true
 
-    attr[:root] ||= (Process.uid.zero? && Dir.home == '/root')
-    raise 'Do not run this as root, use sudo instead' if attr[:root] == true
+      attr[:root] ||= (Process.uid.zero? && Dir.home == '/root')
+      raise 'Do not run this as root, use sudo instead' if attr[:root] == true
 
-    mkdirs(backup_dir, dest_dir)
+      mkdirs(backup_dir, dest_dir)
 
-    copy_config_dir(backup_dir, dest_dir, attr)
+      copy_config_dir(backup_dir, dest_dir, attr)
 
-    puts "dotfiles copied to #{dest_dir}."
-    puts "backups created @ #{backup_dir}."
-  end
-
-  def self.copy_config_dir(backup_dir, dest_dir, attr = {})
-    # Dir.children(CONFIG_DIR).each do |file|, released in ruby 2.5.1
-    # in 2.3.3 which is shipped with babun
-    Dir.foreach(CONFIG_DIR).each do |file|
-      # Explanation of this regexp in test/test_copy_confib.rb
-      # .for_each returns '.' and '..' which we dont want
-      next if file =~ /\A\.{1,2}\Z/
-
-      config = File.join(CONFIG_DIR, file)
-      dot = File.join(dest_dir, ".#{file}")
-      backup = File.join(backup_dir, ".#{file}.orig")
-
-      if OS.linux?
-        copy_unix_files(config, dot, backup)
-        copy_sshd_config(backup_dir) && next if file == 'sshd_config'
-      end
-      copy_cygwin_files(config, dot, backup) if OS.cygwin?
-      # only copies if sudo, linux, and ssh_path exists
-    end
-  end
-
-  def self.sshd_copyable?(ssh_dir = nil, process_uid = nil)
-    process_uid ||= Process.uid.zero?
-    ssh_dir ||= '/etc/ssh'
-
-    not_sudo = 'not running process as sudo, will sshd_config not copied'
-    return puts not_sudo if process_uid != true
-
-    no_ssh_dir = 'No ssh dir found. sshd_config not copied'
-    return puts no_ssh_dir unless Dir.exist?(ssh_dir)
-
-    true
-  end
-
-  def self.copy_sshd_config(backup_dir, sshd_path)
-    return unless sshd_copyable?
-
-    sshd_cfg_path = File.join(CONFIG_DIR, 'sshd_config')
-    sshd_path ||= '/etc/ssh/sshd_config'
-    sshd_backup = File.join(backup_dir, 'sshd_config.orig')
-
-    FileUtils.cp(sshd_path, sshd_backup) if File.exist?(sshd_path)
-    puts "copying to #{sshd_path}"
-    FileUtils.cp(sshd_cfg_path, sshd_path)
-  end
-
-  def self.dot_file_found?(file)
-    return true if File.exist?(file)
-
-    puts "#{file} does not exist. No backup created."
-    false
-  end
-
-  def self.backup_file_not_found?(file)
-    return true unless File.exist?(file)
-
-    puts "#{file} exists already. No backup created."
-    false
-  end
-
-  # helper method to run within a file list
-  def self.copy_unix_files(config_file, dot_file, backup_file)
-    non_unix_files = %w[cygwin_zshrc minttyrc]
-    return if non_unix_files.include?(File.basename(config_file))
-
-    copy_files(config_file, dot_file, backup_file)
-  end
-
-  def self.copy_cygwin_files(config_file, dot_file, backup_file, cygwin = nil)
-    cygwin ||= OS.cygwin?
-    puts 'you are running on cygwin' && return unless cygwin == true
-
-    non_cygwin_files = %w[zshrc]
-    return if non_cygwin_files.include?(File.basename(config_file))
-
-    if File.basename(config_file) == 'cygwin_zshrc'
-      # Converts cygwin_zshrc to .zshrc for cygwin environment use
-      dot_file = File.join(File.dirname(dot_file), '.zshrc')
-      backup_file = File.join(File.dirname(backup_file), '.zshrc.orig')
+      puts "dotfiles copied to #{dest_dir}."
+      puts "backups created @ #{backup_dir}."
     end
 
-    copy_files(config_file, dot_file, backup_file)
-  end
+    def self.copy_config_dir(backup_dir, dest_dir, attr = {})
+      # Dir.children(CONFIG_DIR).each do |file|, released in ruby 2.5.1
+      # in 2.3.3 which is shipped with babun
+      Dir.foreach(CONFIG_DIR).each do |file|
+        # Explanation of this regexp in test/test_copy_confib.rb
+        # .for_each returns '.' and '..' which we dont want
+        next if file =~ /\A\.{1,2}\Z/
 
-  def self.copy_files(config_file, dot_file, backup_file)
-    # if there is an original dot file & no backup file in the backupdir
-    if dot_file_found?(dot_file)
-      if backup_file_not_found?(backup_file)
-        # Copy the dot file to the backup dir
-        FileUtils.cp(dot_file, backup_file)
+        config = File.join(CONFIG_DIR, file)
+        dot = File.join(dest_dir, ".#{file}")
+        backup = File.join(backup_dir, ".#{file}.orig")
+
+        if OS.linux?
+          copy_unix_files(config, dot, backup)
+          copy_sshd_config(backup_dir) && next if file == 'sshd_config'
+        end
+        copy_cygwin_files(config, dot, backup) if OS.cygwin?
+        # only copies if sudo, linux, and ssh_path exists
       end
     end
 
-    # Copies from vps-setup/config to home_dir
-    FileUtils.cp(config_file, dot_file)
-  end
+    def self.sshd_copyable?(ssh_dir = nil, process_uid = nil)
+      process_uid ||= Process.uid.zero?
+      ssh_dir ||= '/etc/ssh'
 
-  def self.mkdirs(*dirs)
-    dirs.each { |dir| FileUtils.mkdir_p(dir) unless Dir.exist?(dir) }
+      not_sudo = 'not running process as sudo, will sshd_config not copied'
+      return puts not_sudo if process_uid != true
+
+      no_ssh_dir = 'No ssh dir found. sshd_config not copied'
+      return puts no_ssh_dir unless Dir.exist?(ssh_dir)
+
+      true
+    end
+
+    def self.copy_sshd_config(backup_dir, sshd_path)
+      return unless sshd_copyable?
+
+      sshd_cfg_path = File.join(CONFIG_DIR, 'sshd_config')
+      sshd_path ||= '/etc/ssh/sshd_config'
+      sshd_backup = File.join(backup_dir, 'sshd_config.orig')
+
+      FileUtils.cp(sshd_path, sshd_backup) if File.exist?(sshd_path)
+      puts "copying to #{sshd_path}"
+      FileUtils.cp(sshd_cfg_path, sshd_path)
+    end
+
+    def self.dot_file_found?(file)
+      return true if File.exist?(file)
+
+      puts "#{file} does not exist. No backup created."
+      false
+    end
+
+    def self.backup_file_not_found?(file)
+      return true unless File.exist?(file)
+
+      puts "#{file} exists already. No backup created."
+      false
+    end
+
+    # helper method to run within a file list
+    def self.copy_unix_files(config_file, dot_file, backup_file)
+      non_unix_files = %w[cygwin_zshrc minttyrc]
+      return if non_unix_files.include?(File.basename(config_file))
+
+      copy_files(config_file, dot_file, backup_file)
+    end
+
+    def self.copy_cygwin_files(config_file, dot_file, backup_file, cygwin = nil)
+      cygwin ||= OS.cygwin?
+      puts 'you are running on cygwin' && return unless cygwin == true
+
+      non_cygwin_files = %w[zshrc]
+      return if non_cygwin_files.include?(File.basename(config_file))
+
+      if File.basename(config_file) == 'cygwin_zshrc'
+        # Converts cygwin_zshrc to .zshrc for cygwin environment use
+        dot_file = File.join(File.dirname(dot_file), '.zshrc')
+        backup_file = File.join(File.dirname(backup_file), '.zshrc.orig')
+      end
+
+      copy_files(config_file, dot_file, backup_file)
+    end
+
+    def self.copy_files(config_file, dot_file, backup_file)
+      # if there is an original dot file & no backup file in the backupdir
+      if dot_file_found?(dot_file)
+        if backup_file_not_found?(backup_file)
+          # Copy the dot file to the backup dir
+          FileUtils.cp(dot_file, backup_file)
+        end
+      end
+
+      # Copies from vps-setup/config to home_dir
+      FileUtils.cp(config_file, dot_file)
+    end
+
+    def self.mkdirs(*dirs)
+      dirs.each { |dir| FileUtils.mkdir_p(dir) unless Dir.exist?(dir) }
+    end
   end
 end
--- lib/vps_setup/pull.rb
+++ lib/vps_setup/pull.rb
@@ -1,40 +1,42 @@
 # frozen_string_literal: true
 
 require 'fileutils'
-require 'copy_config'
 require 'os'
 
-# Pull changes from local dir into config dir to be able to push changes up to repo
-class PullChanges
-  # Must use foreach due to not having Dir.children in 2.3.3 for babun
-  def self.linux_config_dotfiles
-    CONFIG_DIR.reject { |file| NON_LINUX_DOTFILES.include?(file) }
-    # only returns dotfiles for linux
-  end
 
-  def self.cygwin_config_dotfiles
-    CONFIG_DIR.reject { |file| NON_CYGWIN_DOTFILES.include(file) }
-    # returns cygwin dotfiles
-  end
-
-  def self.cygwin_local_dotfiles(attr = {})
-    cygwin_files.map do |file|
-      file = 'zshrc' if file == 'cygwin_zshrc' # need to convert for use with babun / cygwin
-      file.prepend('.')
+module VPS_Setup
+  # Pull changes from local dir into config dir to be able to push changes up to repo
+  class PullChanges
+    # Must use foreach due to not having Dir.children in 2.3.3 for babun
+    def self.linux_config_dotfiles
+      CONFIG_DIR.reject { |file| NON_LINUX_DOTFILES.include?(file) }
+      # only returns dotfiles for linux
     end
-  end
 
-  def self.linux_local_dotfiles(attr = {})
-    linux_config_dotfiles.map { |file| file.prepend('.') }
-  end
+    def self.cygwin_config_dotfiles
+      CONFIG_DIR.reject { |file| NON_CYGWIN_DOTFILES.include(file) }
+      # returns cygwin dotfiles
+    end
 
-  def self.copy_sshd_config
-    sshd_local_path = '/etc/ssh/sshd_config'
-    sshd_config_path = File.join(CONFIG_DIR, 'sshd_config')
-    FileUtils.cp(sshd_local_path, sshd_config_path)
-  end
+    def self.cygwin_local_dotfiles(attr = {})
+      cygwin_files.map do |file|
+        file = 'zshrc' if file == 'cygwin_zshrc' # need to convert for use with babun / cygwin
+        file.prepend('.')
+      end
+    end
 
-  def self.copy_gnome_terminal_settings
+    def self.linux_local_dotfiles(attr = {})
+      linux_config_dotfiles.map { |file| file.prepend('.') }
+    end
 
+    def self.copy_sshd_config
+      sshd_local_path = '/etc/ssh/sshd_config'
+      sshd_config_path = File.join(CONFIG_DIR, 'sshd_config')
+      FileUtils.cp(sshd_local_path, sshd_config_path)
+    end
+
+    def self.copy_gnome_terminal_settings
+
+    end
   end
 end
