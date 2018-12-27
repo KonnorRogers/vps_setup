2e81707cff295cb43f5f331155562ce4b3f8e575
= <konnor5456@gmail.com>
2018-12-27 09:13:19 +0000

    Working on tests for pull

--- lib/vps_setup/pull.rb
+++ lib/vps_setup/pull.rb
@@ -2,42 +2,117 @@
 
 require 'fileutils'
 require 'os'
+require 'rake'
 
 # NON_DOTFILES & variation constants defined in lib/vps_setup.rb
 
 module VpsSetup
-  # Pull changes from local dir into config dir to be able to push changes up to repo
+  # Pull changes from local dir into config dir
+  # to be able to push changes up to the config dir
   class Pull
+    def self.pull_all(attr = {})
+      pull_all_linux(attr) if OS.linux?
+      pull_all_cygwin(attr) if OS.cygwin?
+    end
+
+    def self.pull_all_linux(attr = {})
+      linux_config_dotfiles_ary(attr[:cfg_dir]).each do |config|
+        linux_local_dotfiles_ary(attr[:local_dir]).each do |local|
+
+          FileUtils.cp(local, config) if config.prepend('.') == local
+        end
+      end
+
+      pull_sshd_config(attr[:sshd_local], attr[:sshd_config])
+      pull_gnome_term_settings(attr[:gnome_local], attr[:gnome_config])
+    end
+
+    def self.pull_all_cygwin(attr = {})
+      cygwin_config_dotfiles_ary(attr[:cfg_dir]).each do |config|
+        cygwin_local_dotfiles_ary(attr[:local_dir]).each do |local|
+          FileUtils.cp(local, config) if config.prepend('.') == local
+
+          puts "Copying #{local} to #{config}"
+
+          next unless config == 'cygwin_zshrc' && local == '.zshrc'
+
+          attr[:cfg_dir] ||= CONFIG_DIR
+          cygwin_zshrc = File.join(attr[:cfg_dir], 'cygwin_zshrc')
+          FileUtils.cp(local, cygwin_zshrc)
+          puts "Copying #{local} to #{cygwin_zshrc}"
+          attr[:cfg_dir] = nil
+        end
+      end
+
+    end
+
     # Must use foreach due to not having Dir.children in 2.3.3 for babun
-    def self.linux_config_dotfiles_ary(cfg_dir = CONFIG_DIR)
-      cfg_dir.reject { |file| NON_LINUX_DOTFILES.include?(file) }
+    def self.linux_config_dotfiles_ary(dir = CONFIG_DIR)
+      Dir.entries(dir).reject do |file|
+        NON_LINUX_DOTFILES.include?(file) || file =~ /\A\.{1,2}\Z/
+      end
       # only returns dotfiles for linux
     end
 
-    def self.cygwin_config_dotfiles_ary(cfg_dir = CONFIG_DIR)
-      cfg_dir.reject { |file| NON_CYGWIN_DOTFILES.include?(file) }
+    def self.cygwin_config_dotfiles_ary(dir = CONFIG_DIR)
+      Dir.entries(dir).reject do |file|
+        # removes '.' && '..'
+        NON_CYGWIN_DOTFILES.include?(file) || file =~ /\A\.{1,2}\Z/
+      end
       # returns cygwin dotfiles
     end
 
+    # *local_dotfiles_ary returns the files w/ a '.', ex: .vimrc
     def self.cygwin_local_dotfiles_ary(local_dir = Dir.home)
-      cygwin_files.map do |file|
-        file = 'zshrc' if file == 'cygwin_zshrc' # need to convert for use with babun / cygwin
-        file.prepend('.')
+      cygwin_config_dotfiles_ary(local_dir).map do |file|
+        # need to convert for use with babun / cygwin
+        if file == 'cygwin_zshrc'
+          '.zshrc'
+        else
+          file.prepend('.')
+        end
       end
     end
 
     def self.linux_local_dotfiles_ary(local_dir = Dir.home)
-      linux_config_dotfiles.map { |file| file.prepend('.') }
+      linux_config_dotfiles_ary(local_dir).map { |file| file.prepend('.') }
     end
 
-    def self.copy_sshd_config(sshd_local_path = '/etc/ssh/sshd_config')
+    def self.pull_sshd_config(sshd_local_path = nil, sshd_config_path = nil)
       sshd_local_path ||= '/etc/ssh/sshd_config'
-      sshd_config_path = File.join(CONFIG_DIR, 'sshd_config')
+      sshd_config_path ||= File.join(CONFIG_DIR, 'sshd_config')
+
+      # checks that there is a file to copy
+      error = "#{sshd_local_path} does not exist. sshd_config not copied to cfg"
+      return puts error unless File.exist?(sshd_local_path)
+
       FileUtils.cp(sshd_local_path, sshd_config_path)
     end
 
-    def self.copy_gnome_terminal_settings
-      # needs testing
+    def self.pull_gnome_term_settings(local_term = nil, config_term = nil)
+      local_term ||= '/org/gnome/terminal/'
+      config_term ||= File.join(CONFIG_DIR, 'gnome_terminal_settings')
+
+      local_error = "#{local_term} does not exist. No copy created of gnome settings"
+      return puts local_error unless File.exist?(local_term)
+
+      config_error = "#{config_term} does not exist. Gnome settings not pulled."
+      return puts config_error unless File.exist?(config_term)
+
+      gnome_dump(local_term, config_term)
+    end
+
+    def self.gnome_dump(local_term, config_term)
+      Rake.sh("dconf dump #{local_term} > #{config_term}")
+    rescue RuntimeError
+      puts 'it appears you dont have dconf installed.'
+      puts 'skipping dumping of gnome_settings'
+      puts 'To install dconf, simply use'
+      puts 'sudo apt-get install dconf-tools'
+      false
+    else
+      puts "Gnome settings successfully dumped into #{config_term}"
+      true
     end
   end
 end
--- test/test_pull.rb
+++ test/test_pull.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'os'
 
-SAMPLE_CONFIG_DIR =
+TEST_CONFIG_FILES =
   %w[cygwin_zshrc
      gnome_terminal_settings
      minttyrc
@@ -12,35 +13,133 @@ SAMPLE_CONFIG_DIR =
      vimrc
      zshrc].freeze
 
-SAMPLE_LINUX_HOME_DIR = %w[.zshrc .pryrc .tmux.conf .vimrc].freeze
-SAMPLE_CYGWIN_HOME_DIR = %w[.zshrc .minttyrc .pryrc .tmux.conf .vimrc].freeze
+TEST_LINUX_LOCAL_DOTFILES = %w[.zshrc .pryrc .tmux.conf .vimrc].freeze
+TEST_CYGWIN_LOCAL_DOTFILES = %w[.zshrc .minttyrc .pryrc .tmux.conf .vimrc].freeze
 
-CYGWIN_CFG_DOTFILES = %w[cygwin_zshrc minttyrc pryrc tmux.conf vimrc].freeze
-LINUX_CFG_DOTFILES = %w[zshrc pryrc tmux.conf vimrc].freeze
+TEST_CYGWIN_CFG_DOTFILES = %w[cygwin_zshrc minttyrc pryrc tmux.conf vimrc].freeze
+TEST_LINUX_CFG_DOTFILES = %w[zshrc pryrc tmux.conf vimrc].freeze
+
+PULL_CONFIG_DIR = File.join(TEST_ROOT, 'pull_config')
+PULL_LOCAL_DIR = File.join(TEST_ROOT, 'pull_test')
 
 class TestPull < Minitest::Test
   include VpsSetup
 
-  def setup; end
+  def setup
+    mk_dirs(PULL_CONFIG_DIR, PULL_LOCAL_DIR)
+    TEST_CONFIG_FILES.each { |file| new_file(PULL_CONFIG_DIR, file) }
+  end
 
-  def teardown; end
+  def new_file(dir, file_name)
+    File.new(File.join(dir, file_name), 'w+')
+  end
+
+  def teardown
+    rm_dirs(PULL_CONFIG_DIR, PULL_LOCAL_DIR)
+  end
 
   def test_linux_config_dotfiles_ary
-    ary = Pull.linux_config_dotfiles_ary(SAMPLE_CONFIG_DIR)
+    ary = Pull.linux_config_dotfiles_ary(PULL_CONFIG_DIR)
 
-    assert_equal(ary.sort, LINUX_CFG_DOTFILES.sort)
+    assert_equal(ary.sort, TEST_LINUX_CFG_DOTFILES.sort)
     # sorting doesnt matter, only used to ensure equality
     # alternatively
     # ary.each { |file| assert_includes(LINUX_CFG_DOTFILES, file) }
   end
 
   def test_cygwin_config_dotfiles_ary
-    ary = Pull.cygwin_config_dotfiles_ary(SAMPLE_CONFIG_DIR)
+    ary = Pull.cygwin_config_dotfiles_ary(PULL_CONFIG_DIR)
 
-    assert_equal(ary.sort, CYGWIN_CFG_DOTFILES)
+    assert_equal(ary.sort, TEST_CYGWIN_CFG_DOTFILES.sort)
   end
 
   def test_cygwin_local_dotfiles_ary
-    ary = Pull.cygwin_local_dotfiles_ary(SAMPLE_HOME_DIR)
+    ary = Pull.cygwin_local_dotfiles_ary(PULL_CONFIG_DIR)
+
+    assert_equal(ary.sort, TEST_CYGWIN_LOCAL_DOTFILES.sort)
+  end
+
+  def test_linux_local_dotfiles_ary
+    ary = Pull.linux_local_dotfiles_ary(PULL_CONFIG_DIR)
+
+    assert_equal(ary.sort, TEST_LINUX_LOCAL_DOTFILES.sort)
+  end
+
+  def test_pull_sshd_config
+    sshd_local_path = File.join(PULL_LOCAL_DIR, 'sshd_config')
+    sshd_config_path = File.join(PULL_CONFIG_DIR, 'sshd_config')
+
+    # prevents from writing to console to not clog up the test suite
+    capture_io do
+      assert_nil(Pull.pull_sshd_config(sshd_local_path, sshd_config_path))
+    end
+    refute(File.exist?(sshd_local_path))
+
+    file = new_file(PULL_LOCAL_DIR, 'sshd_config')
+    file.write('testing')
+
+    Pull.pull_sshd_config(sshd_local_path, sshd_config_path)
+
+    assert(File.exist?(sshd_config_path))
+    assert(File.exist?(sshd_local_path))
+    assert_equal File.read(file), File.read(sshd_config_path)
+    assert_equal File.read(file), File.read(sshd_local_path)
+  end
+
+  def test_pull_gnome_term_settings
+    skip('You are not running on linux, this test will fail') unless OS.linux?
+    local_term = File.join(PULL_LOCAL_DIR, 'gnome_terminal_settings')
+    config_term = File.join(PULL_CONFIG_DIR, 'gnome_terminal_settings')
+
+    Pull.pull_gnome_term_settings(local_term, config_term)
+
+    refute File.exist?(local_term)
+    assert File.exist?(config_term)
+
+    unless File.exist?('/org/gnome/terminal')
+      skip('You do not have /org/gnome/terminal, dconf will not work')
+    end
+    config_file = File.new('gnome_settings')
+    FileUtils.sh("dconf dump /org/gnome/terminal > #{config_file}")
+    Pull.pull_gnome_term_settings(local_term, config_term)
+
+    assert_equal File.read(local_term), File.read(config_term)
+  end
+
+  def test_pull_all_cygwin
+    attr = {
+      cfg_dir: PULL_CONFIG_DIR,
+      local_dir: PULL_LOCAL_DIR
+    }
+
+    # pulls the real config_zshrc from the config dir for File read purpose
+    config_zshrc = File.new(File.join(CONFIG_DIR, 'cygwin_zshrc'), 'r+')
+    # test_config_zshrc = File.join(PULL_CONFIG_DIR, 'cygwin_zshrc')
+
+    contents = File.read(config_zshrc)
+
+    local_zshrc = File.join(PULL_LOCAL_DIR, '.zshrc')
+    File.open(local_zshrc, 'w+') { |file| file.write(contents) }
+
+    # zshrc_file = File.join(PULL_LOCAL_DIR, 'not_getting_pulled')
+    # File.open(File.join(zshrc_file), 'w+') do |file|
+    #   file.write(contents)
+    # end
+
+    dir = 'test_dir'
+    Dir.foreach(CONFIG_DIR) do |file|
+      next if file =~ /\A\.{1,2}\Z/
+
+      file_path = File.join(dir, file)
+      File.open(file_path, 'w+') { |f| f.write(file.read) }
+    end
+
+    Pull.pull_all_cygwin(attr)
+
+    # config_zshrc.rewind
+    # assert_equal File.read(local_zshrc), File.read(zshrc_file)
+    # assert_equal File.read(test_config_zshrc), File.read(zshrc_file)
+
   end
 end
+
