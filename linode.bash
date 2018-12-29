874016bcf1ff96785d3d87aa7c62907d8f311885
= <konnor5456@gmail.com>
2018-12-29 06:59:12 +0000

    Added imagemagick to apt install

--- linode.bash
+++ linode.bash
@@ -15,7 +15,7 @@ get_dependencies() {
     sudo apt upgrade -y 
     sudo apt autoremove -y 
 
-    PACKAGE_LIST="curl software-properties-common tmux git vim zsh gnupg2 sqlite3 postgresql less python3 python3-pip python-dev python3-dev python-pip ufw pry ack-grep libfuse2 fuse python3-neovim build-essential bison zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libffi-dev nodejs apt-transport-https ca-certificates golang make gcc ruby-dev rubygems fail2ban httpie"
+    PACKAGE_LIST="curl software-properties-common tmux git vim zsh gnupg2 sqlite3 postgresql less python3 python3-pip python-dev python3-dev python-pip ufw pry ack-grep libfuse2 fuse python3-neovim build-essential bison zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev libffi-dev nodejs apt-transport-https ca-certificates golang make gcc ruby-dev rubygems fail2ban httpie imagemagick"
 
     for item in $PACKAGE_LIST; do
       sudo apt install $item -y
