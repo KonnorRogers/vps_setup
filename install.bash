d22bc4881e38adc9c9e811eafe7f0ac5ded37aac
= <konnor5456@gmail.com>
2019-01-04 07:01:27 +0000

    finishing touches

--- install.bash
+++ install.bash
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [[ $HOME == '/root' ]]; then
+  echo "Please add a user to run this script as"
+  read -p 'Username: ' uservar
+  adduser $uservar
+  adduser $uservar sudo
+  echo "Please rerun this script as the new user" 1>&2
+  exit 1
+fi
+
 if [[ $OSTYPE == 'linux-gnu' ]]; then
   sudo apt-get install ruby
 elif [[ $OSTYPE == 'cygwin' ]]; then
