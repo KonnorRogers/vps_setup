27c0b246b55ef1d5da6e5825b54b75f594b5e056
paramagicdev <konnor5456@gmail.com>
2019-01-17 06:13:53 +0000

    removing packages repeat

--- lib/vps_setup/packages.rb
+++ lib/vps_setup/packages.rb
@@ -2,12 +2,6 @@
 
 module VpsSetup
   class Packages
-    LIBS = %w[software-properties-common gnupg2 less ufw npm
-              ack-grep libfuse2 apt-transport-https
-              ca-certificates build-essential bison
-              zlib1g-dev libyaml-dev libssl-dev
-              libgdbm-dev libreadline-dev libffi-dev fuse make gcc].freeze
-
     LANGUAGES = %w[python3 python3-pip python-dev
                    python3-dev python-pip python3-neovim
                    nodejs golang ruby ruby-dev].freeze
@@ -19,6 +13,6 @@ module VpsSetup
     ADDED_REPOS = %w[neovim asciinema docker-ce mosh yarn].freeze
 
     # Does not include gems
-    UBUNTU = LIBS.dup.concat(LANGUAGES).concat(TOOLS).concat(ADDED_REPOS)
+    UBUNTU = LANGUAGES.dup.concat(TOOLS).concat(ADDED_REPOS)
   end
 end
