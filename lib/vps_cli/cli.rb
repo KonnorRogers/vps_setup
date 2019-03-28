949c7a267a6cab1e6e845d4193a8e55db803c739
paramagicdev <konnor5456@gmail.com>
2019-03-28 02:30:02 +0000

    modifying and tweaking

--- lib/vps_cli/cli.rb
+++ lib/vps_cli/cli.rb
@@ -10,17 +10,19 @@ module VpsCli
     # this is available as a flag for all methods
     class_option :verbose, type: :boolean, aliases: :v, default: true
     class_option interactive: :boolean, aliases: :i, default: true
-    class_options [:local_dir, :backup_dir, :local_sshd_config]
+    class_option all: :boolean, aliases: :a, default: false
 
-    desc 'copy_all', 'Copies files from <vps_cli/config_files>'
-    def copy_all
-      VpsCli::Copy.all(options)
+    class_options %i[local_dir backup_dir local_sshd_config]
+
+    desc 'copy [OPTIONS]', 'Copies files from <vps_cli/config_files>'
+    def copy
+      VpsCli::Copy.all(options.dup) if options[:all]
     end
 
-    desc 'pull_all', 'Pulls files into your vps_cli repo'
-    options [:dotfiles_dir, :misc_files_dir]
-    def pull_all
-      VpsCli::Pull.all(options.dup)
+    desc 'pull [OPTIONS]', 'Pulls files into your vps_cli repo'
+    options %i[dotfiles_dir misc_files_dir]
+    def pull
+      VpsCli::Pull.all(options.dup) if options[:all]
     end
   end
 end
