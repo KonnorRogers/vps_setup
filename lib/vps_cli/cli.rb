4d0bebab18e2323ca8f452b469bb089fe8fff901
paramagicdev <konnor5456@gmail.com>
2019-03-28 02:09:55 +0000

    added cli.rb components

--- lib/vps_cli/cli.rb
+++ lib/vps_cli/cli.rb
@@ -7,14 +7,20 @@ module VpsCli
   # Integrates Thor
   # @see http://whatisthor.com/
   class Cli < Thor
-    # # this is available as a flag for all methods
-    # class_option :verbose, type: :boolean
+    # this is available as a flag for all methods
+    class_option :verbose, type: :boolean, aliases: :v, default: true
+    class_option interactive: :boolean, aliases: :i, default: true
+    class_options [:local_dir, :backup_dir, :local_sshd_config]
 
-    # desc 'Copies all files from <vps_cli/config_files> to <local_dir>'
-    # options :local_dir, :backup_dir, :local_sshd_config
-    # option interactive: :boolean, aliases: :i, default: true
-    # def copy_all
-    #   VpsCli::Copy.all(options)
-    # end
+    desc 'copy_all', 'Copies files from <vps_cli/config_files>'
+    def copy_all
+      VpsCli::Copy.all(options)
+    end
+
+    desc 'pull_all', 'Pulls files into your vps_cli repo'
+    options [:dotfiles_dir, :misc_files_dir]
+    def pull_all
+      VpsCli::Pull.all(options)
+    end
   end
 end
